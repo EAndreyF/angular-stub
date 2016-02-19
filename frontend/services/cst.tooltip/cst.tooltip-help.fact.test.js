0bdabed1f774582946f721e2833759efebffcd70
Andrey Evstropov <genesis.andrey@yandex.ru>
2016-02-19 19:51:48 +0300

    add first test

---
+++ frontend/services/cst.tooltip/cst.tooltip-help.fact.test.js
@@ -0,0 +1,5 @@
+describe('first test', function () {
+  it('try', function() {
+    expect(true).toBe(true);
+  });
+});
---
+++ karma.conf.js
@@ -0,0 +1,92 @@
+
+
+module.exports = function (config) {
+
+  var conf = {
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: '',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['bower', 'jasmine', 'angular-filesort'],
+
+    bowerPackages: [
+      'angular',
+      'angular-mocks'
+    ],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+      'frontend/**/*.js'
+    ],
+
+    angularFilesort: {
+      whitelist: [
+        'frontend/**/*.js'
+      ]
+    },
+
+
+    // list of files to exclude
+    exclude: [],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {},
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['story'],
+
+    storyReporter: {
+      showSkipped: true, // default: false
+      showSkippedSummary: true  // default: false
+    },
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['PhantomJS'],
+    //browsers: [process.env.NODE_ENV === 'continuous' ? 'Firefox' : 'Chrome'],
+
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+    //singleRun: false,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  };
+
+
+  if (process.env.NODE_ENV === 'test' || process.env.NODE_ENV === 'continuous') {
+    conf.singleRun = true;
+  }
+
+  config.set(conf);
+};
