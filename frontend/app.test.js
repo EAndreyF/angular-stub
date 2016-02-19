33fac227cb7e50ff31a08ca31b55a9497af587a4
Andrey Evstropov <genesis.andrey@yandex.ru>
2016-02-19 20:03:02 +0300

    add factory tests

---
+++ frontend/app.test.js
@@ -0,0 +1,5 @@
+describe('check tests', function () {
+  it('work', function() {
+    expect(true).toBe(true);
+  });
+});
--- frontend/services/cst.tooltip/cst.tooltip-help.fact.test.js
+++ frontend/services/cst.tooltip/cst.tooltip-help.fact.test.js
@@ -1,5 +1,17 @@
-describe('first test', function () {
-  it('try', function() {
-    expect(true).toBe(true);
-  });
+describe('cstTooltipHelpFct', function () {
+  var helper;
+
+  beforeEach(module('cst.tooltip'));
+
+  beforeEach(inject(function (cstTooltipHelpFct) {
+    helper = cstTooltipHelpFct;
+  }));
+
+  describe('.placeInBoard', function () {
+
+    it('is function' , function () {
+      expect(typeof helper.placeInBoard).toBe('function');
+    })
+
+  })
 });
